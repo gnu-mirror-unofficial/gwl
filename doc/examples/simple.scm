679fd3084d4d19b6b42f98d193d179bb93f19b3d
Ricardo Wurmus <rekado@elephly.net>
2018-12-18 12:58:18 +0100

    doc: Add simple example.
    * doc/examples/simple.scm: New file.
    * doc/gwl.texi (A Simple Workflow): New section.
    * Makefile.am (EXTRA_DIST): Distribute new file.

---
+++ doc/examples/simple.scm
@@ -0,0 +1,48 @@
+(define-module (simple)
+  #:use-module (guix processes)
+  #:use-module (guix workflows)
+  #:use-module (gnu packages base))
+
+(define-public greet
+  (process
+    (name "greet")
+    (package-inputs (list hello))
+    (procedure '(system "hello"))))
+
+(define-public sleep
+  (process
+   (name "sleep")
+   (package-inputs (list coreutils))
+    (procedure
+     '(begin
+        (display "Sleeping...\n")
+        (system "sleep 10")))))
+
+(define-public (eat something)
+  (process
+    (name "eat")
+    (procedure
+     `(format #t "Eating ~a\n" ,something))))
+
+(define-public bye
+  (process
+    (name "bye")
+    (procedure
+     '(display "Farewell, world!\n"))))
+
+(define-public simple
+  (let ((eat-fruit (eat "fruit"))
+        (eat-veges (eat "vegetables")))
+    (workflow
+     (name "simple")
+     (processes
+      (list greet
+            eat-fruit
+            eat-veges
+            sleep
+            bye))
+     (restrictions
+      `((,eat-fruit ,greet)
+        (,eat-veges ,greet)
+        (,sleep ,eat-fruit ,eat-veges)
+        (,bye ,sleep))))))
