cc83f8a921a6101c8518c2fd23f5c35ce61c0632
Ricardo Wurmus <rekado@elephly.net>
2019-01-16 12:49:04 +0100

    gwl: Simplify graph sorting.
    * gwl/workflows/execution-order.scm (find-roots): New procedure.
    (compute-free-points, resolve-dependencies, resolve-dependency,
    full-dependency-list): Remove procedures.
    (parallel-step-execution-order, sequential-execution-order): Express
    in terms of find-roots.

--- gwl/workflows/execution-order.scm
+++ gwl/workflows/execution-order.scm
@@ -1,4 +1,5 @@
 ;;; Copyright © 2016, 2018 Roel Janssen <roel@gnu.org>
+;;; Copyright © 2019 Ricardo Wurmus <rekado@elephly.net>
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify it
 ;;; under the terms of the GNU General Public License as published by
@@ -15,78 +16,47 @@
 
 (define-module (gwl workflows execution-order)
   #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match)
   #:export (sequential-execution-order
-            parallel-step-execution-order
-            compute-free-points
-            resolve-dependency
-            resolve-dependencies))
+            parallel-step-execution-order))
 
-(define (resolve-dependency process dependencies)
-  "Remove PROCESS from the list of DEPENDENCIES."
-  ;; Remove the resolved dependency.
-  (let ((reduced (map (lambda (pair)
-                        `(,(car pair) .
-                          ,(if (cdr pair)
-                               (delete process (cdr pair)) #f)))
-                      dependencies)))
-    ;; Set the empty pairs to #f, so that it is equivalant to
-    ;; a missing key.
-    (map (lambda (pair)
-           (let ((pair-dependencies (cdr pair)))
-             `(,(car pair) . ,(if (null? pair-dependencies)
-                                  #f
-                                  pair-dependencies))))
-         reduced)))
-
-(define (resolve-dependencies processes dependencies)
-  "Removes PROCESSES from the list of DEPENDENCIES."
-  (if (null? processes)
-      dependencies
-      (resolve-dependencies
-       (cdr processes)
-       (resolve-dependency (car processes) dependencies))))
-
-(define (full-dependency-list processes dependencies)
-  "Returns a list of dependency pairs, including implicit dependency pairs."
-  (map (lambda (process)
-         `(,process . ,(assoc-ref dependencies process)))
-       processes))
-
-(define (compute-free-points processes dependencies)
-  "Returns a list of processes that can run immediately."
-  (map car (remove list? (full-dependency-list processes dependencies))))
-
-(define* (sequential-execution-order processes dependencies #:optional (order '()))
-  "Returns the list of PROCESSES, re-ordered so it can be executed in a sequence
-and adhere to the dependencies provided in DEPENDENCIES."
-  (if (null? processes)
-    (reverse order)
-    (let* ((resolvable (compute-free-points processes dependencies))
-           (reduced-dependencies (resolve-dependencies resolvable dependencies))
-           (leftovers (lset-difference eq? processes resolvable)))
-      (if (null? resolvable)
-          #f
-          (sequential-execution-order
-           leftovers
-           (resolve-dependencies resolvable dependencies)
-           (append resolvable order))))))
+(define (find-roots nodes dependencies)
+  "Return the roots of the graph spanned by NODES and the adjacency
+list DEPENDENCIES.  As a second value, return the other NODES and a
+new adjacency list without them."
+  (let ((roots (fold (lambda (node roots)
+                       (match (assoc-ref dependencies node)
+                         ((or #f ()) (cons node roots))
+                         (_ roots)))
+                     '() nodes)))
+    (values roots
+            (lset-difference eq? nodes roots)
+            (map
+             (match-lambda
+               ((source . targets)
+                (cons source (lset-difference eq? targets roots))))
+             (lset-difference eq? dependencies roots)))))
 
 (define (parallel-step-execution-order processes dependencies)
-  "Returns the list of PROCESSES grouped by processes that can be executed in
-parallel at each step and adhere to the dependencies provided in DEPENDENCIES."
+  "Returns the list of PROCESSES grouped by processes that can be
+executed in parallel at each step and adhere to the dependencies
+provided in DEPENDENCIES."
+  (let loop ((processes processes)
+             (dependencies dependencies)
+             (result '()))
+    (call-with-values
+        (lambda ()
+          (find-roots processes dependencies))
+      (lambda (roots processes dependencies)
+        (match roots
+          (() (reverse result))
+          (_  (loop processes dependencies
+                    (cons roots result))))))))
 
-  (define (stepper processes dependencies order)
-    (if (null? processes)
-        (reverse order)
-        (let* ((resolvable (compute-free-points processes dependencies))
-               (leftovers (lset-difference eq? processes resolvable)))
-          (if (null? resolvable)
-              #f
-              (stepper leftovers
-                       (resolve-dependencies resolvable dependencies)
-                       (append (list resolvable) order))))))
-
-  (stepper processes dependencies '()))
+(define (sequential-execution-order processes dependencies)
+  "Returns the list of PROCESSES, re-ordered so it can be executed in a sequence
+and adhere to the dependencies provided in DEPENDENCIES."
+  (apply append (parallel-step-execution-order processes dependencies)))
 
 (define* (graph-execution-order processes dependencies #:optional (order '()))
   "Returns the list of PROCESSES, ordered such that each list contains a
