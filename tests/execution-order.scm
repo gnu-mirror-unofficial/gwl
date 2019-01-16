32bb19dc0ae70a8e2927555111fd73925ecaa4de
Ricardo Wurmus <rekado@elephly.net>
2019-01-16 22:09:33 +0100

    tests: Add test for graph sorting.
    * tests/execution-order.scm: New file.
    * Makefile.am (SCM_TESTS): Add it.

---
+++ tests/execution-order.scm
@@ -0,0 +1,79 @@
+;;; Copyright © 2019 Ricardo Wurmus <rekado@elephly.net>
+;;;
+;;; This program is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (test-execution-order)
+  #:use-module (gwl processes)
+  #:use-module (gwl workflows)
+  #:use-module (gwl workflows execution-order)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
+  #:use-module (srfi srfi-64))
+
+(test-begin "execution-order")
+
+;; 1 -> 2 -\
+;;   -> 3 -> 5 --> 6
+;; 4 ------/
+
+(define p1 (process (name "p1") (procedure '())))
+(define p2 (process (name "p2") (procedure '())))
+(define p3 (process (name "p3") (procedure '())))
+(define p4 (process (name "p4") (procedure '())))
+(define p5 (process (name "p5") (procedure '())))
+(define p6 (process (name "p6") (procedure '())))
+
+(define wf
+  (workflow
+   (name "test-workflow")
+   (processes
+    (list p1 p2 p3 p4 p5 p6))
+   (restrictions
+    (list (list p2 p1)
+          (list p3 p1)
+          (list p5 p2 p3 p4)
+          (list p6 p5)))))
+
+(test-assert "sequential-execution-order"
+  (let* ((order (sequential-execution-order
+                 (workflow-processes wf)
+                 (workflow-restrictions wf)))
+         (first-then (lambda (a b)
+                       (call-with-values
+                           (lambda ()
+                             (partition (cut eq? a <>) order))
+                         (lambda (before after)
+                           (find (cut eq? b <>) after))))))
+    (and (first-then p1 p2)
+         (first-then p1 p3)
+         (first-then p1 p5)
+         (first-then p1 p6)
+         (first-then p4 p5)
+         (first-then p4 p6)
+         (first-then p2 p5)
+         (first-then p2 p6)
+         (first-then p3 p5)
+         (first-then p3 p6))))
+
+(test-assert "parallel-step-execution-order"
+  (every (lambda (a b) (lset= eq? a b))
+         (parallel-step-execution-order
+          (workflow-processes wf)
+          (workflow-restrictions wf))
+         (list (list p1 p4)
+               (list p2 p3)
+               (list p5)
+               (list p6))))
+
+(test-end "execution-order")
