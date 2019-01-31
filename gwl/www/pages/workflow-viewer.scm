503caffe537ccd9a92207c86e2f1723d0cffd04b
Ricardo Wurmus <rekado@elephly.net>
2019-01-31 02:43:27 +0100

    www: Generate SVG only if it doesn't exist yet.
    * gwl/www/pages/workflow-viewer.scm (workflow-graph-svg-object): Only
    generate SVG when it doesn't exist yet.

--- gwl/www/pages/workflow-viewer.scm
+++ gwl/www/pages/workflow-viewer.scm
@@ -22,24 +22,25 @@
   #:use-module (gwl workflows graph)
   #:use-module (gwl www pages)
   #:use-module (gwl www config)
+  #:use-module (ice-9 match)
   #:use-module (ice-9 vlist)
   #:export (page-workflow-viewer))
 
 (define (workflow-graph-svg-object workflow-name)
   "Return the SXML to render an SVG containing the graph of WORKFLOW-NAME."
-  (let ((workflow-list (find-workflow-by-full-name workflow-name)))
-    (if (null? workflow-list)
-        `(p "Sorry, I could not render graph.")
-        (let* ((workflow (car workflow-list))
-               (web-path (string-append "/static/graphs/" workflow-name ".svg"))
-               (dot-file (string-append (web-config 'static-root) "/graphs/" workflow-name))
-               (svg-file (string-append dot-file ".svg")))
-          (with-output-to-file dot-file
-            (lambda _
-              (display (workflow->dot workflow))))
-          (system* (web-config 'dot) "-Tsvg" dot-file (string-append "-o" svg-file))
-          `(img (@ (src ,web-path)
-                   (style "max-height: 100%; max-width: 100%")))))))
+  (match (find-workflow-by-full-name workflow-name)
+    (() `(p "Sorry, I could not render graph."))
+    ((workflow . _)
+     (let* ((web-path (string-append "/static/graphs/" workflow-name ".svg"))
+            (dot-file (string-append (web-config 'static-root) "/graphs/" workflow-name))
+            (svg-file (string-append dot-file ".svg")))
+       (unless (file-exists? svg-file)
+         (with-output-to-file dot-file
+           (lambda _
+             (display (workflow->dot workflow))))
+         (system* (web-config 'dot) "-Tsvg" dot-file (string-append "-o" svg-file)))
+       `(img (@ (src ,web-path)
+                (style "max-height: 100%; max-width: 100%")))))))
 
 (define* (page-workflow-viewer request-path #:key (post-data ""))
   (let* ((workflows (fold-workflows
