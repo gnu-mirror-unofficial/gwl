9ab51f0b318a0de1a5df7af492b3a836edd07d2b
Ricardo Wurmus <rekado@elephly.net>
2019-06-12 13:56:20 +0200

    doc: Do not define and return workflow.
    * doc/examples/extended-example-workflow.scm (extended-dynamic-workflow):
    Remove variable definition.

--- doc/examples/extended-example-workflow.scm
+++ doc/examples/extended-example-workflow.scm
@@ -17,23 +17,20 @@
               (time 10)))
    (procedure # bash { gzip --list {{inputs}} > {{outputs}} })))
 
-(define-public extended-dynamic-workflow
-  (let* (;; Get all processes of the other workflow.
-         (foreign-processes (workflow-processes dynamic-workflow))
+(let* (;; Get all processes of the other workflow.
+       (foreign-processes (workflow-processes dynamic-workflow))
 
-         ;; Get the processes that we want to extend on.
-         (compress-file-processes (processes-filter-by-name
-                                   foreign-processes "compress-file"))
+       ;; Get the processes that we want to extend on.
+       (compress-file-processes (processes-filter-by-name
+                                 foreign-processes "compress-file"))
 
-         ;; Create the new processes.
-         (list-file-processes (map list-file-template
-                                   (map (compose first process-outputs)
-                                        compress-file-processes))))
-    (workflow
-     (name "extended-dynamic-workflow")
-     (processes
-      (append
-       (workflow-restrictions dynamic-workflow)
-       (zip list-file-processes compress-file-processes))))))
-
-extended-dynamic-workflow
+       ;; Create the new processes.
+       (list-file-processes (map list-file-template
+                                 (map (compose first process-outputs)
+                                      compress-file-processes))))
+  (workflow
+   (name "extended-dynamic-workflow")
+   (processes
+    (append
+     (workflow-restrictions dynamic-workflow)
+     (zip list-file-processes compress-file-processes)))))
