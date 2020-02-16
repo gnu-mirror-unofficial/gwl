094bc667531d7e36034c8d92bcfe92af5623506c
Ricardo Wurmus <rekado@elephly.net>
2020-02-16 15:33:49 +0100

    www: Expand description.
    * gwl/www/pages/welcome.scm (page-welcome): Expand on the description
    of what the GWL is about.

--- gwl/www/pages/welcome.scm
+++ gwl/www/pages/welcome.scm
@@ -19,20 +19,41 @@
   #:use-module (gwl www pages)
   #:export (page-welcome))
 
-(define %gwl-repository "https://git.savannah.gnu.org/cgit/gwl.git/tree/")
-
 (define (page-welcome request-path)
   (page-root-template "Guix Workflow Language" request-path
    `((h2 "A workflow management language extension for GNU Guix")
 
-     (p "The Guix Workflow Language (GWL) provides an extension to GNU Guix's "
+     (p "The Guix Workflow Language (GWL) provides a scientific
+computing extension to GNU Guix's "
         (a (@ (href "https://arxiv.org/abs/1305.4584"))
-           "declarative language for package management") " to automate "
-           "execution of programs.  Additionally, GWL can use "
-           (a (@ (href ,(string-append %gwl-repository "gwl/process-engines")))
-              "process engines") " to integrate with various computing "
-              "environments.  As of February 11, 2018, GWL is a part of "
-              (a (@ (href "https://www.gnu.org")) "GNU") ".")
+           "declarative language for package management")
+        " for the declaration of scientific workflows.")
+
+     (p "It combines the specification of work units and their
+relationship to one another with the reproducible software deployment
+facilities of the functional package manager GNU Guix.  A GWL workflow
+will always run in a reproducible environment that GNU Guix
+automatically prepares.  The GWL extends your Guix installation with a
+single new sub-command: " (code "guix workflow") ".")
+
+     (p "In the GWL there are two concepts we need to know about:
+processes and workflows.  We describe a computation (running a
+program, or evaluating a Scheme expression) using a process.  A
+workflow describes how individual processes relate to each
+other (e.g. "
+        (em "process B must run after process A, and process C must
+run before process A")
+        ").")
+
+     (p "GWL workflows are executable code.  The workflow language is
+embedded in the powerful general purpose language "
+        (a (@ (href "https://gnu.org/software/guile/"))
+           "Guile Scheme")
+        ", so you can compute arbitrarily complex process and workflow
+definitions.  The GWL supports a classic Lisp syntax as well as a
+Python-like syntax called "
+        (a (@ (href "https://www.draketo.de/light/english/wisp-lisp-indentation-preprocessor"))
+           "Wisp") ".")
 
      (div (@ (style "text-align: center;"))
           (div (@ (class "action-button"))
