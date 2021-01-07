357fb7025c953664f4b661492a4d244b80f3760c
Ricardo Wurmus <rekado@elephly.net>
2021-01-07 08:12:38 +0100

    Update manual header.

--- cfg.mk
+++ cfg.mk
@@ -1,10 +1,33 @@
 
 manual_title = Guix Workflow Language
-_header='<div id=header><div id=header-inner class=width-control><div class=logo><img  src=\"/static/images/logo.png\"></div><div class=menu><ul><li><a href=/>Home</a></li><li class=active>Manual</li></ul></div></div></div><div id=content class=width-control>'
+_header='\
+<div id=\"wrapper\">\
+  <div id=\"header\">\
+    <div id=\"header-inner\" class=\"width-control\">\
+      <div class=\"logo\">\
+        <img  src=\"/static/images/logo.png\" alt=\"Guix Workflow Language\">\
+      </div>\
+      <div class=\"menu\">\
+        <ul>\
+          <li><a href=\"/\">Home</a></li>\
+          <li><a href=\"/tutorial\">Tutorial</a></li>\
+          <li class=active>Manual</li>\
+          <li><a href=\"/community\">Community</a></li>\
+        </ul>\
+      </div>\
+    </div>\
+  </div>\
+  <div id=content class=width-control>'
 
 gendocs_options_ = \
   -I $(abs_srcdir)/build-aux \
-  --html "--no-number-sections --css-ref=/static/css/main.css -c TOP_NODE_UP_URL=/manual -c AFTER_BODY_OPEN=$(_header) -c PRE_BODY_CLOSE='</div>' -c TOC_LINKS=true  -c 'EXTRA_HEAD=<meta name=\"viewport\" \ content=\"width=device-width, initial-scale=1\" />'"
+  --html "--no-number-sections \
+--css-ref=/static/css/main.css \
+-c TOP_NODE_UP_URL=/manual \
+-c AFTER_BODY_OPEN=$(_header) \
+-c PRE_BODY_CLOSE='</div></div>' \
+-c TOC_LINKS=true \
+-c 'EXTRA_HEAD=<meta name=\"viewport\" \ content=\"width=device-width, initial-scale=1\" />'"
 
 gpg_key_ID = BCA689B636553801C3C62150197A5888235FACAC
 my-release-prep:
